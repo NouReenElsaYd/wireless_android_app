93e98efd776f1cd31e04e91ab3137435099c73c6
Noureen Elsayd <noureenelsayd@gmail.com>
2024-12-03 01:06:28 +0200

    first commit

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -0,0 +1,54 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import com.example.myapplication.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ActivityLoginBinding binding;
+    DatabaseHelper databaseHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        databaseHelper = new DatabaseHelper(this);
+
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = binding.loginEmail.getText().toString();
+                String password = binding.loginPassword.getText().toString();
+
+                if(email.isEmpty() || password.isEmpty())
+                    Toast.makeText(LoginActivity.this, "Email or Password mustn't be empty", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkCredentials = databaseHelper.checkEmailPassword(email, password);
+
+                    if(checkCredentials){
+                        Toast.makeText(LoginActivity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
+                        // Modified: Change to redirect to ProfileActivity
+                        Intent intent  = new Intent(getApplicationContext(), ProfileActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(LoginActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        binding.signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
